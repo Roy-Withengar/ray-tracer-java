c4b5533a0fb80b3beb15afd3a2e59f2678c75caa
Withengar-aaa <1664653117@qq.com>
2026-06-08 16:43:31 +0800

    视角坐标转画布

--- src/main/java/com/rast/Point2D.java
+++ src/main/java/com/rast/Point2D.java
@@ -1,7 +1,37 @@
 package com.rast;
 
-public record Point2D(int x,int y,double h)
-{
+import com.demo.PixelDrawer;
+import com.demo.Point;
 
+public record Point2D(int x, int y, double h)
+{
+    static  int canvasWidth = 600;
+
+    static  int canvasHeight = 600;
+
+    static  int viewSize = 2;
+
+    static  int projectionPlaneZ = 1;
+
+
+
+    public static Point2D viewportToCanvas(double x ,double y)
+    {
+        double scaledX = x * canvasWidth / viewSize;
+
+        double scaledY = y * canvasHeight / viewSize;
+
+        return new Point2D((int) scaledX, (int) scaledY, 1.0);
+    }
+
+    public static Point2D projectVertex(Point point)
+    {
+
+        double projX = point.getX() * projectionPlaneZ / point.getZ();
+
+        double projY = point.getY() * projectionPlaneZ / point.getZ();
+
+        return viewportToCanvas(projX, projY);
+    }
 
 }
