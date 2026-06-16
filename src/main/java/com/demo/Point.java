6b2dd4a8d7ca644b0720d40ccd052013fe1810dc
Withengar-aaa <1664653117@qq.com>
2026-06-16 17:08:50 +0800

    增加方法

--- src/main/java/com/demo/Point.java
+++ src/main/java/com/demo/Point.java
@@ -25,7 +25,15 @@ public class Point
         return new Vector3D(point.x,point.y,point.z);
     }
 
+    public static Point addPoint(Point p1,Point p2)
+    {
+        return new Point(p1.x + p2.x,p1.y + p2.y,p1.z + p2.z);
+    }
 
+    public static Point multiply(int k,Point point)
+    {
+        return new Point(k * point.x,k * point.y,k * point.z);
+    }
 
 
 
