0e69cbd6a315543e51402203ebd701ca7e057bcb
Withengar-aaa <1664653117@qq.com>
2026-06-16 17:09:32 +0800

    公共配置类

---
+++ src/main/java/com/rast/RenderConfig.java
@@ -0,0 +1,17 @@
+package com.rast;
+
+public class RenderConfig
+{
+    private RenderConfig() {}
+
+    // 画布尺寸（全局唯一）
+    public static final int CANVAS_WIDTH = 600;
+
+    public static final int CANVAS_HEIGHT = 600;
+
+    // 视口大小、投影平面Z（透视投影参数）
+    public static final int VIEW_SIZE = 1;
+
+    public static final int PROJECTION_PLANE_Z = 1;
+
+}
