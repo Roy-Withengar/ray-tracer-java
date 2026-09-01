ec5bf6bd8b52a130a1f1813d0fe7181156ea68d7
Withengar-aaa <1664653117@qq.com>
2026-09-01 15:01:28 +0800

    添加常量做全局设定

--- src/main/java/com/rast/RenderConfig.java
+++ src/main/java/com/rast/RenderConfig.java
@@ -14,4 +14,24 @@ public class RenderConfig
 
     public static final int PROJECTION_PLANE_Z = 1;
 
+    public static final boolean UseVertexNormals = true;
+
+    // 着色模型常量，和 JS 变量名完全对齐
+    public static final int SM_FLAT = 0;
+
+    public static final int SM_GOURAUD = 1;
+
+    public static final int SM_PHONG = 2;
+
+    // 当前使用的着色模型
+    public static final int SHADING_MODEL = SM_PHONG;
+
+    // ========== 光照掩码 新增 ==========
+
+    public static final int LM_DIFFUSE  = 1 << 0;
+
+    public static final int LM_SPECULAR = 1 << 1;
+
+    // 当前光照模式：漫反射 + 高光，等价 JS: LM_DIFFUSE | LM_SPECULAR
+    public static int LIGHTING_MODEL = LM_DIFFUSE | LM_SPECULAR;
 }
