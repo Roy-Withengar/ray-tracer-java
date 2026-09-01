03cda724a44a6c2a356fb407896fd32f62148b57
Withengar-aaa <1664653117@qq.com>
2026-09-01 14:52:19 +0800

    更改数据类型

--- src/main/java/com/rast/ObjectInstance.java
+++ src/main/java/com/rast/ObjectInstance.java
@@ -2,7 +2,7 @@ package com.rast;
 
 import com.demo.Point;
 
-public record ObjectInstance(ObjectRender objectRender, Point point,Matrix4 orientation,double scale)
+public record ObjectInstance(Object objectRender, Point point,Matrix4 orientation,double scale)
 {
 
 
