7e964ddfe29e962740e3ed514a48d232bbf442d2
Withengar-aaa <1664653117@qq.com>
2026-09-01 14:58:37 +0800

    添加剪切平面

--- src/main/java/com/rast/RastCamera.java
+++ src/main/java/com/rast/RastCamera.java
@@ -2,7 +2,10 @@ package com.rast;
 
 import com.demo.Point;
 
+import java.util.List;
+
 public record  RastCamera(Point position,Matrix4 orientation)
 {
+    public static List<Plane> clippingPlanes ;
 
 }
